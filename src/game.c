da65014ee213c6d93fb2a82646c523d9f34fb748
DannyDLux90 <d.hergenhahn@gmx.de>
2026-09-10 17:33:56 +0200

    Add v0.60 split game translation unit

---
+++ src/game.c
@@ -0,0 +1,7 @@
+/* Generated split translation unit: edit the .inc parts. */
+#include "game_parts/01.inc"
+#include "game_parts/02.inc"
+#include "game_parts/03.inc"
+#include "game_parts/04.inc"
+#include "game_parts/05.inc"
+#include "game_parts/06.inc"

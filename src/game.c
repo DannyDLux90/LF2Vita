95913e7ea62b495ffdc9d89811a7b7019b340e2f
DannyDLux90 <d.hergenhahn@gmx.de>
2026-09-10 18:53:26 +0200

    Switch combat runtime to v0.62 mode-aware core

--- src/game.c
+++ src/game.c
@@ -1,7 +1,7 @@
-/* Generated split translation unit: edit the .inc parts. */
-#include "game_parts/01.inc"
-#include "game_parts/02.inc"
-#include "game_parts/03.inc"
-#include "game_parts/04.inc"
-#include "game_parts/05.inc"
-#include "game_parts/06.inc"
+/* LF2Vita v0.62 generated runtime split. */
+#include "runtime62/game_01.inc"
+#include "runtime62/game_02.inc"
+#include "runtime62/game_03.inc"
+#include "runtime62/game_04.inc"
+#include "runtime62/game_05.inc"
+#include "runtime62/game_06.inc"

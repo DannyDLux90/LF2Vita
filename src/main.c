10ed869f62fbebc85501d274bd244b6993b036a2
DannyDLux90 <d.hergenhahn@gmx.de>
2026-09-11 10:53:18 +0200

    fix3: split current main runtime

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
-/* LF2Vita v0.62 generated runtime split. */
-#include "runtime62/main_01.inc"
-#include "runtime62/main_02.inc"
-#include "runtime62/main_03.inc"
-#include "runtime62/main_04.inc"
-#include "runtime62/main_05.inc"
-#include "runtime62/main_06.inc"
+/* LF2Vita v0.69 generated runtime split. */
+#include "runtime69/main_01.inc"
+#include "runtime69/main_02.inc"
+#include "runtime69/main_03.inc"
+#include "runtime69/main_04a.inc"
+#include "runtime69/main_04b.inc"
+#include "runtime69/main_04c.inc"
